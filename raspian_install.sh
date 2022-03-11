e179d4cd6e1324a038f8c6cb20ea7173efa6646f
danf42 <31356438+danf42@users.noreply.github.com>
2022-03-11 06:11:43 -0500

    Update ntp.conf

--- raspian_install.sh
+++ raspian_install.sh
@@ -180,17 +180,12 @@ cat > /etc/ntp.conf << EOF
 driftfile /var/lib/ntp/ntp.drift
 logfile /var/log/ntp.log
 
-restrict default kod nomodify notrap nopeer noquery
-restrict -6 default kod nomodify notrap nopeer noquery
-restrict 127.0.0.1 mask 255.255.255.0
-restrict -6 ::1
-
 # GPS Serial data reference (NTP0)
-server 127.127.28.0
-fudge 127.127.28.0 flag1 1 refid GPS
+server 127.127.28.0 minpoll 4 maxpoll 4
+fudge 127.127.28.0 time1 0.0 flag1 1 refid GPS
 
 # GPS PPS reference (NTP1)
-server 127.127.28.1 prefer
+server 127.127.28.1 minpoll 4 maxpoll 4 prefer
 fudge 127.127.28.1 flag1 1 refid PPS
 EOF
 
